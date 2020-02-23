60f2e006401cf0eccef72dc0553b3b2eb032a2eb
Anekenonso <okenwanonsokenneth@gmail.com>
2020-02-23 22:35:41 +0100

    set up the javascript files

---
+++ scripts/scripts/server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const app = express();
+
+app.listen(4000, ()=> {
+    console.log('server running on port 4000');
+});
+
+app.get('/download', (req, res) => {
+    var URL = reg.query.URL;
+    res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+
+    ytdl(URL, {
+        format: 'mp4'
+    }).pipe(res);
+});
