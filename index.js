703822e27a9b6a53d943359452674d6d256ac778
anekenonso <okenwanonsokenneth@gmail.com>
2020-04-11 22:37:11 +0100

    modification

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
+
 const app = express();
+
+app.use(cors());
 
 app.listen(4000, ()=> {
     console.log('server running on port 4000');
 });
 
-app.get('/download', (req, res) => {
+// get mp4 file download
+app.get('/downloadmp4', (req, res) => {
     var URL = req.query.URL;
     res.header('Content-Disposition', 'attachment; filename="video.mp4"');
 
     ytdl(URL, {
         format: 'mp4'
     }).pipe(res);/*.pipe(fs.createWriteStream('video.flv'));*/
+});
+
+
+// get mp3 download
+app.get('/downloadmp3', (req,res) => {
+    var URL = req.query.URL;
+    res.header('Content-Disposition', 'attachment; filename="audio.mp3" ');
+    ytdl(URL, {
+        format: 'mp3',
+        filter: 'audioonly'
+    }).pipe(res);
 });
--- main.js
+++ main.js
@@ -1,18 +1,32 @@
-var convertBtn = document.querySelector('.convert-button');
+var mp3Btn = document.getElementById('mp3');
+var mp4Btn = document.getElementById('mp4');
+//var convertBtn = document.querySelector('.convert-button');
 var URLinput = document.querySelector('.URL-input');
 
-convertBtn.addEventListener('click', () => {
+
+//server to connect to for download
+var server = 'http://localhost:4000';
+
+
+// convert to mp3
+mp3Btn.addEventListener('click', () => {
     console.log(`URL: ${URLinput.value}`);
-      sendURL(URLinput.value);
+      redirectMp3(URLinput.value);
 });
 
-function sendURL(URL) {
-    
-  fetch(`https://yvd.netlify.com/?URL=${URL}`, {
-    method: 'GET'
-  }).then(res => res.json())
-  .then(json => console.log(json));
-  
 
-  window.location.href = `http://localhost:4000/download?URL=${URL}`;
+//convert to mp4
+mp4Btn.addEventListener('click', () =>{
+      console.log(`URL: ${URLinput.value}`);
+      redirectMp4(URLinput.value);
+});
+
+//getting mp3
+function redirectMp3(query) {
+  window.location.href = `${server}/downloadmp3?url=${query}`;
+}
+
+// getting mp4
+function redirectMp4(query) {
+  window.location.href = `${server}/downloadmp4?url=${query}`;
 }
